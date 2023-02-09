328baa1b4b30023ffed6a76d559c56a33936e347
Alex Marquez <marquezworld@gmail.com>
2023-02-09 20:43:41 +0100

    Skillz Hooks React

---
+++ src/App/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import QuestionsRouter from "../QuestionsRouter/QuestionsRouter";
+import Intro from "../Intro/Intro";
+import Outro from "../Outro/Outro";
+import "./App.css";
+
+const App = () => {
+
+    const [questionsAndAnswers, setQuestionsandAnswers] = useState([]);
+}
+
+const onLast = (newQuestionsAndAnswers) => {
+    setQuestionsandAnswers(newQuestionsAndAnswers)
+}
+
+return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/vragen/:number" element={<QuestionsRouter onLast={onLast}/>}/>
+            <Route path="/outro" element={<Outro questionsAndAnswers={questionsAndAnswers}/>}/>
+            <Route path="/" element={<Intro/>}></Route>
+        </Routes>
+    </BrowserRouter>
+);
+
+
+export default App;
